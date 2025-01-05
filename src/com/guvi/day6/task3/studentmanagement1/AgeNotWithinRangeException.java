44c7592d400decff9ad83f01fd663663f9e71b23
JOSEPH KIRUBAKARAN <josephdevkiruba@gmail.com>
2025-01-05 23:06:13 +0530

    DAY 6 Task 3 Day 8 Task 4

---
+++ src/com/guvi/day6/task3/studentmanagement1/AgeNotWithinRangeException.java
@@ -0,0 +1,7 @@
+package com.guvi.day6.task3.studentmanagement1;
+
+class AgeNotWithinRangeException extends Exception {
+    public AgeNotWithinRangeException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/guvi/day6/task3/studentmanagement1/NameNotValidException.java
@@ -0,0 +1,7 @@
+package com.guvi.day6.task3.studentmanagement1;
+
+class NameNotValidException extends Exception {
+    public NameNotValidException(String message) {
+        super(message);
+    }
+}
